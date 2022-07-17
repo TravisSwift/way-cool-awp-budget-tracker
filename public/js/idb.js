178335c3db4d7e17ed49503395db0563b5c94f59
Travis Swift <travis.arthur.swift@gmail.com>
2022-07-17 11:17:08 -0500

    commit

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,17 +1,23 @@
+const { response } = require("express");
+const { ServerResponse } = require("http");
+
 // create variable to hold db connection
 let db;
-// establish a connection to IndexedDB database called 'pizza_hunt' and set it to version 1
-const request = indexedDB.open('pizza_hunt', 1);
+// establish a connection to IndexedDB database
+const request = indexedDB.open('budget=tracker', 1);
 
-// upon a successful ?
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore("bank_transaction", { autoIncremente: true});
+};
+
+
 request.onsuccess = function(event) {
-    // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+
     db = event.target.result;
   
-    // check if app is online, if yes run uploadPizza() function to send all local db data to api
     if (navigator.onLine) {
-      // we haven't created this yet, but we will soon, so let's comment it out for now
-      // uploadPizza();
+      uploadBankTransaction();
     }
   };
   
@@ -23,10 +29,56 @@ request.onsuccess = function(event) {
  // This function will be executed if we attempt to submit a new pizza and there's no internet connection
  function saveRecord(record) {
     // open a new transaction with the database with read and write permissions 
-    const transaction = db.transaction(['new_pizza'], 'readwrite');
+    const transaction = db.transaction(['bank_transaction'], 'readwrite');
+
+    const bankObjectStore = transation(["bank_transaction"], "readwrite");
+
+    bankObjectStore.add(record);
+ }
+
+    function uploadBankTransaction() {
+      const transaction = db.transaction(["bank_transaction"], "readwrite");
+    
   
-    // access the object store for `new_pizza`
-    const pizzaObjectStore = transaction.objectStore('new_pizza');
+    // access the object store
+    const bankObjectStore = transaction.objectStore('bank_transaction');
+
+    const getAll = bankObjectStore.getAll();
+
+    getAll.onsuccess = function () {
+      if (getAll.result.length > 0) {
+        fetch("api/transaction/bulk", {
+          method: "POST", 
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+          }
+        })
+        .then(response => response.json())
+        .then(ServerResponse => {
+          if (ServerResponse.message) {
+            throw new Error(ServerResponse);
+          }
+
+
+          const transaction = db.transaction(["bank_transaction"], "readwrite");
+
+          const bankObjectStore = transaction.objectStore("bank_transaction");
+
+          bankObjectStore.ckear();
+
+          alert("Saved transactions have been submitted");
+        })
+
+        .catch(err => {
+          console.log(err);
+        });        
+      }
+    };
+    };
+
+    window.addEventListener('online', uploadBankTransaction);
   
-    // add record to your store with add method
-    pizzaObjectStore.add(record);
+    // // add record to your store with add method
+    // pizzaObjectStore.add(record);
